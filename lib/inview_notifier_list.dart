c0e421532823accedc4ade0472937d60b5488c59
zhangjingyuan <jingyuan.zhang@shopcider.com>
2023-09-05 11:35:43 +0800

    don't hide InviewNotifier

--- lib/inview_notifier_list.dart
+++ lib/inview_notifier_list.dart
@@ -1,3 +1,4 @@
+export 'src/inherited_inview_widget.dart';
+export 'src/inview_notifier.dart';
 export 'src/inview_notifier_list.dart';
 export 'src/inview_state.dart';
-export 'src/inview_notifier.dart' hide InViewNotifier;
